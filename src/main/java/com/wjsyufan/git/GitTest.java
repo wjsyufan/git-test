7cb82cf3b1ced0c1daf81d5b496a948505dedab0
yufan <yufan@tfrunning.com.cn>
2022-07-05 16:37:51 +0800

    master 3st commit

--- src/main/java/com/wjsyufan/git/GitTest.java
+++ src/main/java/com/wjsyufan/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git11!");
         System.out.println("hello git22!");
+        System.out.println("hello git33!");
     }
 }
